3c0e5bacafe357e098cb52be10bb550745fa8b2d
Grapo <vipsoldierFROMolympiack@protonmail.com>
2021-12-29 13:00:22 -0300

    Chnaged dependencies for my_first_mod mod id

--- src/main/java/com/github/grapoisvip/my_first_mod/MyFirstMod.java
+++ src/main/java/com/github/grapoisvip/my_first_mod/MyFirstMod.java
@@ -24,14 +24,16 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("my_first_mod")
-public class ExampleMod
+@Mod(MyFirstMod.MOD_ID)
+public class MyFirstMod
 {
+	public static final String MOD_ID = "my_first_mod";
+	
     // Directly reference a log4j logger.
 	public static final RegistryObject<Item> BOW = RegistryObject.of(new ResourceLocation("minecraft:bow"), ForgeRegistries.ITEMS);
     private static final Logger LOGGER = LogManager.getLogger();
 
-    public ExampleMod() {
+    public MyFirstMod() {
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         // Register the enqueueIMC method for modloading
