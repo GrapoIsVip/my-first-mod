da6db96b4c08cc89889bae03fa2d8aa601c04163
Grapo <vipsoldierFROMolympiack@protonmail.com>
2021-12-30 23:04:04 -0300

    registry screwdriver tool

--- src/main/java/com/github/grapoisvip/my_first_mod/core/ItemInit.java
+++ src/main/java/com/github/grapoisvip/my_first_mod/core/ItemInit.java
@@ -3,8 +3,11 @@ package com.github.grapoisvip.my_first_mod.core;
 import com.github.grapoisvip.my_first_mod.MyFirstMod;
 
 import net.minecraft.item.BlockItem;
+import net.minecraft.item.IItemTier;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemTier;
+import net.minecraft.item.ToolItem;
 import net.minecraft.item.Item.Properties;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
@@ -18,4 +21,7 @@ public class ItemInit {
 	
 	public static final RegistryObject<BlockItem> ANDROANTHUS_SAPLING = ITEMS.register("androanthus_sapling",
 			() -> new BlockItem(BlockInit.ANDROANTHUS_SAPLING.get(), new Properties().tab(ItemGroup.TAB_DECORATIONS)));
+	
+	public static final RegistryObject<Item> SCREWDRIVER_TOOL = ITEMS.register("screwdriver_tool",
+			() -> new ToolItem(1f, 0, ItemTier.IRON, null, new Properties().tab(ItemGroup.TAB_TOOLS)));
 }
